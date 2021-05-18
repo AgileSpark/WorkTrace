f49fb5845126546e44cd103e9cc4dc5187072c77
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-05-18 18:12:11 -0400

    Add files via upload
    Updated boiler plate code
    
    
    Co-authored-by: peterhyesung <peterhyesung@users.noreply.github.com>
    Co-authored-by: dieunity <dieunity@users.noreply.github.com>
    Co-authored-by: MadinventorZero <MadinventorZero@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: '/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    publicPath: '/',
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+};
