62a59959a4b73d6cb5bc8a9d926e3100587690b9
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-05-18 20:05:14 -0400

    Final boiler plate update, working extension once build:prod script is ran and dist folder is unpacked into chrome
    Co-authored-by: peterhyesung <peterhyesung@users.noreply.github.com>
    Co-authored-by: dieunity <dieunity@users.noreply.github.com>
    Co-authored-by: MadinventorZero <MadinventorZero@users.noreply.github.com>

---
+++ src/components/Popup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Popup() {
+    return (
+        <div style={styles.main}>
+            <h1>Chrome Ext - Popup</h1>
+        </div>
+    )
+}
+const styles = {
+    main: {
+        width: '600px',
+        height: '600px'
+    }
+}
+export default Popup;
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,72 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: '/client/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  devServer: {
-    publicPath: '/',
-    compress: true,
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './client/index.html',
-    }),
-  ],
-  resolve: {
-    // Enable importing JS / JSX files without specifying their extension
-    extensions: ['.js', '.jsx'],
-  },
-};
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
+        historyApiFallback: true
+    },
+    entry: {
+        popup: path.resolve(__dirname, "./src/index-popup.js"),
+        options: path.resolve(__dirname, "./src/index-options.js"),
+        foreground: path.resolve(__dirname, "./src/index-foreground.js")
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react',
+                                {
+                                    'plugins': ['@babel/plugin-proposal-class-properties']
+                                }
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'popup.html',
+            template: 'src/popup.html',
+            chunks: ['popup']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'options.html',
+            template: 'src/options.html',
+            chunks: ['options']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'foreground.html',
+            template: 'src/foreground.html',
+            chunks: ['foreground']
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/manifest.json', to: '[name].[ext]' },
+                { from: 'src/background.js', to: '[name].[ext]' },
+                { from: 'src/inject_script.js', to: '[name].[ext]' },
+                { from: 'src/assets/*.png', to: '[name].[ext]' }
+            ]
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
