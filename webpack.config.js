438b1993e3048c1c951bff194303d4f869bfd38e
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-05-18 22:23:38 -0400

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+// test
 module.exports = {
     devServer: {
         port: '8080',
