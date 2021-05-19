4a7420832e73387de25939341dfb910704b2a9a5
MadinventorZero <42157117+MadinventorZero@users.noreply.github.com>
2021-05-18 19:36:24 -0700

    Server Skeleton & Routes
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: peterhyesung <peterhyesung@users.noreply.github.com>
    Co-authored-by: dieunity <dieunity@users.noreply.github.com>

---
+++ server/controllers/sessionController.js
@@ -0,0 +1,55 @@
+const db = require ('../schemas/schemaSQL');
+
+const sessionController = {};
+
+sessionController.setCookie = (req, res, next) => {
+  try {
+    res.cookie('WorkTrace', Math.floor(Math.random() * 99).toString(), {
+      httpOnly: true,
+      secure: true,
+      maxAge: 360000
+    });
+    return next();
+  } catch (err) {next(err);}
+};
+
+sessionController.setSSIDCookie = (req, res, next) => {
+  try {
+    res.cookie('SSID', Math.floor(Math.random() * 99).toString(), {
+      httpOnly: true,
+      secure: true,
+      maxAge: 360000
+    });
+    return next();
+  } catch (err) {next(err);}
+};
+
+sessionController.loggedIn = (req, res, next) => {
+  const sessionQuery = 
+  `SELECT ***
+  FROM ****
+  `;
+  try {
+  db.query(sessionQuery, (err, result) => {
+    const {rows} = result;
+    res.locals.sessionStatus = rows;
+    return next();
+  });
+  } catch (err) {next(err);}
+};
+
+sessionController.startSession = (req, res, next) => {
+  const sessionQuery = 
+  `SELECT ***
+  FROM ****
+  `;
+  try {
+  db.query(sessionQuery, (err, result) => {
+    const {rows} = result;
+    res.locals.sessionStatus = rows;
+    return next();
+  });
+  } catch (err) {next(err);}
+}
+
+module.exports  = sessionController;
---
+++ server/routes/userRouter.js
@@ -0,0 +1,25 @@
+const userController = require('../controllers/userController');
+const sessionController = require('./sessionController');
+const userRouter = express.router();
+
+// set cookie -> create user -> start session -> set SSID cookie
+userRouter.post('/createUser', sessionController.setCookie, userRouter.createUser, sessionController.startSession, sessionController.setSSIDCookie, (req, res) => {
+
+})
+
+// set cookie -> check if logged In -> verify user (if not loggedin) -> start session (or renew session) - set (or renew) SSID cookie
+userRouter.get('/verifyUser', sessionController.setCookie, sessionController.loggedIn, userRouter.verifyUser, sessionController.startSession, sessionController.setSSIDCookie,  (req, res) => {
+  
+})
+
+// verify user -> update user password
+userRouter.get('/updatePassword', userRouter.verifyUSer, userRouter.updateUserPassword, (req, res) => {
+  
+})
+
+// verify user -> delete user
+userRouter.delete('/deleteUser', userRouter.verifyUser, userRouter.deleteUser, (req, res) => {
+  
+})
+
+module.exports = userRouter;
